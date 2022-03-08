f35f95a39374aa4d13474f494db602f09cfa2fc3
mr-Alarak <82238611+fardin-yp@users.noreply.github.com>
2022-03-08 15:51:10 +0330

    Update server.js

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(bodyParserErrorHandler());
    app.use(express.static("uploads"));
    app.use(cookieparser())
    app.use(cors({
-   origin:["https://dream-web.runflare.run/"],
+   origin:["https://dream-web.runflare.run"],
    credentials:true,
    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"],
    allowedHeaders: ["Content-Type", "Authorization"],
