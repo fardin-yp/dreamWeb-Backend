7a833b6f6861d59f59b35c510100f7689dc1e103
mr-Alarak <82238611+fardin-yp@users.noreply.github.com>
2022-08-01 17:26:16 +0430

    Update server.js

--- server.js
+++ server.js
@@ -26,6 +26,28 @@ const io = require("socket.io")(server ,{
    cors:{origin:"*"}
 })
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+app.configure(function () {
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(allowCrossDomain);
+  app.use(express.static(path.join(application_root, "public")));
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
    const MuiltiPartyMiddleware = multiparty({uploadDir:"../images"});
    app.use(express.json());
    app.set('trust proxy', 1);
